752236c05148e9b2112d17782ae42c6d5049e819
donghonam <rkdl0829@gmail.com>
2024-03-19 22:26:34 +0900

    docker compose setting

---
+++ db/initdb.d/create_database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS membership;
+
+CREATE TABLE membership.membership
+(
+    membership_id  bigint not null auto_increment,
+    name           varchar(255),
+    address        varchar(255),
+    email          varchar(255),
+    is_corporation bit    not null,
+    is_valid       bit    not null,
+    primary key (membership_id)
+) engine=InnoDB
