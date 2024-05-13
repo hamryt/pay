6294111f316b67af3e0f55f051c09355da66647f
donghonam <rkdl0829@gmail.com>
2024-05-13 21:59:13 +0900

    kafka producer, consumer 추가

---
+++ banking-service/src/main/java/com/daehwapay/bankingservice/config/BankingConfig.java
@@ -0,0 +1,9 @@
+package com.daehwapay.bankingservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.daehwapay.common")
+public class BankingConfig {
+}
---
+++ money-service/src/main/java/com/daehwapay/moneyservice/config/MoneyConfig.java
@@ -0,0 +1,9 @@
+package com.daehwapay.moneyservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.daehwapay.common")
+public class MoneyConfig {
+}
